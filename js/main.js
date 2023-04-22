4d70390510b4ff14fa0d1e7c11f2b6c3ab66d741
moxe <moxe228@gmail.com>
2023-04-23 00:07:22 +0300

    d

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //Задание 7 часть 2
 import { arrayUsers } from './data.js';
 import { createThumbnails } from './renderingThumbnails.js';
+import './effect.js';
+import './scale.js'
 
 createThumbnails(arrayUsers);
 
--- js/pic.js
+++ js/pic.js
@@ -5,9 +5,11 @@ const uplCancel = document.querySelector('#upload-cancel');
 uplButton.addEventListener('change', ()=>{
   uplOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
+  imgPreview.style.transform = 'scale(1)';
 });
 
 uplCancel.addEventListener('click', ()=>{
+  resetFilter();
   uplOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
 });
